b050f843ac282806747cebb5b91101d47cc8fa3f
eperazaco <eperaza@prointeccr.com>
2022-07-01 11:34:52 -0600

    [ADD] kp carga de modulo de updates de water

---
+++ waterpump_update/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "water pump update",
+
+    'summary': """
+        Modulo que se encarga de las modificasiones personalizadas de water pump
+    """,
+
+    'description': """
+        -evita que la fecha pueda ser modificada
+        -mueve el campo de ref interna a un lugar mas visible
+    """,
+
+    'author': "PROINTEC",
+    'website': "http://www.prointeccr.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base', 'account', 'contacts'],
+
+    # always loaded
+    'data': [
+        'views/account_move_views.xml',
+        'views/res_partner_views.xml',
+    ],
+}
