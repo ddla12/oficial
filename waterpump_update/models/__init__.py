e7e8149696da111a3d72397c562d0b0b2167117e
eperazaco <eperaza@prointeccr.com>
2022-09-14 09:54:06 -0600

    Impide que se ingreso TC para dolares en 1

--- waterpump_update/models/__init__.py
+++ waterpump_update/models/__init__.py
@@ -3,3 +3,4 @@
 from . import models
 from . import account_invoice
 from . import pos_order
+from . import res_currency_rate
---
+++ waterpump_update/models/res_currency_rate.py
@@ -0,0 +1,19 @@
+
+from odoo import models, fields, api, _
+import datetime
+from odoo.exceptions import UserError, ValidationError
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class xResCurrencyRateUpdate(models.Model):
+    _inherit = 'res.currency.rate'
+
+    @api.constrains('rate')
+    def _check_rate(self):
+        for rec in self:
+            if (((rec.company_id and rec.company_id.currency_id != rec.currency_id and rec.currency_id.name == 'USD')
+                or (not rec.company_id and rec.currency_id.name == 'USD')) and rec.rate > 0.01):
+                raise ValidationError("W: El tipo de cambio para dólares para el día %s es incorrecto.  Valor: %s" % (rec.name.strftime("%d-%b-%Y").replace('.',''), str(rec.rate)) )
