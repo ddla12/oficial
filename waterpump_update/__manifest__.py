90e4a5951ad6989c2e4c06f640094e97e0080d5b
eperazaco <eperaza@prointeccr.com>
2022-10-27 19:26:20 -0600

    Actualiza modulo waterpump update

--- waterpump_update/__manifest__.py
+++ waterpump_update/__manifest__.py
@@ -1,29 +1,19 @@
 # -*- coding: utf-8 -*-
 {
     'name': "water pump update",
-
     'summary': """
         Modulo que se encarga de las modificasiones personalizadas de water pump
     """,
-
     'description': """
-        -evita que la fecha pueda ser modificada
-        -mueve el campo de ref interna a un lugar mas visible
+        +mueve el campo de ref interna a un lugar mas visible
     """,
-
+    'category': 'Extra',
+    'version': '14',
     'author': "PROINTEC",
     'website': "http://www.prointeccr.com",
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    'depends': ['base', 'contacts', 'FAE_app', 'pos_extensionfe', 'purchase'],
-
-    # always loaded
+    'depends': ['base', 'contacts'],
     'data': [
-        'views/account_move_views.xml',
         'views/res_partner_views.xml',
         'views/product_template_views.xml',
-        'views/sale_order_views.xml',
-        'report/pos_order_invoice_template.xml',
     ],
 }
--- waterpump_update/models/__init__.py
+++ waterpump_update/models/__init__.py
@@ -1,8 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from . import models
-from . import account_invoice
-from . import pos_order
 from . import res_currency_rate
-from . import sale_order
 from . import product_template
